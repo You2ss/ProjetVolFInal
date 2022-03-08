0aaa145703887af2f113b9a952b2ba0cd2438746
Youssef <Y.HUSSEIN@lprs.com>
2022-03-08 02:00:13 +0100

    Ajout des fonctions de vol

--- src/modele/Vol_2.php
+++ src/modele/Vol_2.php
@@ -1,6 +1,37 @@
 <?php
-
-class Vol_2
+class Vol
 {
+    private $select;
+
+
+    public function getSelect()
+    {
+        return $this->select;
+    }
+
+    public function setSelect()
+    {
+        $bdd = new BDD();
+        $req = $bdd->setPdo()->prepare('SELECT * FROM vol ');
+        $req->execute(array());
+
+        $donnee = $req->fetchAll();
+        return $donnee;
+    }
+
+    public function __construct(array $donnees){
+        $this->hydrate($donnees);
+    }
+
+    public function hydrate(array $donnees){
+        foreach ($donnees as $key => $value){
+            $method = 'set'.ucfirst($key);
+            //setNom
+            if (method_exists($this, $method)){
+                //On appelle le setter
+                $this->$method($value);
+            }
+        }
+    }
 
 }
