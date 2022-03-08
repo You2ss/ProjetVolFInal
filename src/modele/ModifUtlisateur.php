9ea871ffad762f4cf980d4152b8ced91e8f53f76
Youssef <Y.HUSSEIN@lprs.com>
2022-03-08 01:53:14 +0100

    Ajout de la classe modification d'utilisateur

---
+++ src/modele/ModifUtlisateur.php
@@ -0,0 +1,102 @@
+<?php
+
+class ModifUtilisateur
+{
+    private $id;
+    private $nom;
+    private $email;
+    private $mot_de_passe;
+
+    public function setUpdate(){
+        $bdd = new BDD();
+        $req = $bdd->setPdo()->prepare('Update client set nom = :nom,email = :email,mot_de_passe = :mot_de_passe where id= :id');
+        $req->execute(array(
+            'id' => $this->getId(),
+            'nom' => $this->getNom(),
+            'email' => $this->getEmail(),
+            'mot_de_passe' => $this->getMotDePasse(),
+        ));
+    }
+
+    public function __construct(array $donnees){
+        $this->hydrate($donnees);
+    }
+
+    public function hydrate(array $donnees){
+        foreach ($donnees as $key => $value){
+            $method = 'set'.ucfirst($key);
+            //setNom
+            if (method_exists($this, $method)){
+                //On appelle le setter
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $mail
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMotDePasse()
+    {
+        return $this->mot_de_passe;
+    }
+
+    /**
+     * @param mixed $mot_de_passe
+     */
+    public function setMotDePasse($mot_de_passe)
+    {
+        $this->mot_de_passe = $mot_de_passe;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+
+
+}
