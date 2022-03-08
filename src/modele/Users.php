d7e1ad1146c382541eb94137b7399aee3d87334a
Youssef <Y.HUSSEIN@lprs.com>
2022-03-08 01:59:19 +0100

    Ajout des fonctions de profil

--- src/modele/Users.php
+++ src/modele/Users.php
@@ -1,6 +1,51 @@
 <?php
 
-class Users
+
+class User
 {
+
+    private $connexion;
+    private $email;
+    private $mdp;
+
+    /**
+     * @return mixed
+     */
+    public function getConnexion()
+    {
+        return $this->connexion;
+    }
+
+    /**
+     * @param mixed $connexion
+     */
+    public function setConnexion($email, $mdp)
+    {
+        $bdd = new BDD();
+        $req= $bdd->getBdd()->prepare('SELECT * FROM user email = :email AND mdp = :mdp');
+        $req->execute(array(
+            'email' => $this->email = $email ,
+            'mdp' => $this->mdp = $mdp
+        ));
+
+        $donnee = $req->fetch();
+        return $donnee;
+    }
+
+    public function __construct(array $donnees){
+        $this->hydrate($donnees);
+    }
+
+    public function hydrate(array $donnees){
+        foreach ($donnees as $key => $value){
+            $method = 'set'.ucfirst($key);
+            //setNom
+            if (method_exists($this, $method)){
+                //On appelle le setter
+                $this->$method($value);
+            }
+        }
+    }
+
 
 }
