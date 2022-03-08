a922284b179ab5135eff901aa2260da501e9457d
Youssef <Y.HUSSEIN@lprs.com>
2022-03-08 01:44:48 +0100

    Ajout de la page de la verificaiton par e-mail

---
+++ src/connexion/verif.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+$bdd= new PDO('mysql:host=localhost;port=3306;dbname=projetvol;charset=utf8', 'root', '');
+if(isset($_GET['id']) AND !empty($_GET['id']) AND isset($_GET['cle']) AND !empty($_GET['id'])){
+    $getid = $_GET['id'];
+    $getcle = $_GET['cle'];
+    $recupUser = $bdd->prepare('SELECT * FROM users WHERE id = ? AND cle = ?');
+    $recupUser->execute(array($getid, $getcle));
+    if($recupUser->rowCount() > 0){
+        $userInfo = $recupUser->fetch();
+        if($userInfo['confirme'] != 1){
+            $updateConfimation = $bdd->prepare('UPDATE users SET confirme = ? WHERE id = ?');
+            $updateConfimation->execute(array(1, $getid));
+            $_SESSION ['cle'] = $getcle;
+            header('Location: ../../index.php');
+        }
+        else{
+            $_SESSION ['cle'] = $getcle;
+            header('Location: ../../index.php');
+        }
+    }
+    else{
+        echo "Votre cle ou identifiant est incorrect";
+    }
+}
+else{
+    echo "Aucun utilisateur trouvé";
+}
+?>
