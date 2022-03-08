618681840509840dc5db9719e76a3ad33b9b0098
Youssef <Y.HUSSEIN@lprs.com>
2022-03-08 02:08:11 +0100

    Ajout du traitement des vols

--- src/traitement/traitement_vol.php
+++ src/traitement/traitement_vol.php
@@ -1,1 +1,18 @@
 <?php
+require_once '../BDD/Bdd.php';
+require_once '../Modele/Vol.php';
+
+$ajouter = new Vol(array(   //setters
+
+    'DateDepart'=>$_POST['date_depart'],
+    'HeureArrivee'=>$_POST['heure_arrivee'],
+    'HeureDepart'=>$_POST['heure_depart'],
+    'RefPilote'=>$_POST['ref_pilote'],
+    'RefAvion'=>$_POST['ref_avion']
+));
+
+$ajouter->addVol();
+header('Location: ../../index-2.php');
+$ajouter->Afficher();
+?>
+
